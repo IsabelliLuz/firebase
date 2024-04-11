79f3c26aeb461078ea66e6994bcd853b182bcc04
IsabelliLuz <132668520+IsabelliLuz@users.noreply.github.com>
2024-04-11 15:41:05 -0300

    Add files via upload

---
+++ fireConfig.js
@@ -0,0 +1,14 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyCzOJnip8_PkcxIQ0dwvxHG2byILZ2bX_Q",
+  authDomain: "isabelli-42981.firebaseapp.com",
+  projectId: "isabelli-42981",
+  databaseURL: "https://isabelli-42981-default-rtdb.firebaseio.com/",
+  storageBucket: "isabelli-42981.appspot.com",
+  messagingSenderId: "275699189297",
+  appId: "1:275699189297:web:c4ecbeada5e8234abf3710"
+};
+
+// Initialize Firebase
+
+firebase.initializeApp(firebaseConfig);
+
